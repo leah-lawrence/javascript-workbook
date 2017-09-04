747cb2e4c83288a39eaf9af138471a55ae5ff1bc
James Walker <james.walker@contractor.net>
2017-09-03 20:06:05 -0500

    Tic Tac Toe Rendered by React with Reset

--- 07week/ticTacToe/script.js
+++ 07week/ticTacToe/script.js
@@ -4,24 +4,16 @@ class TicTacToe extends React.Component {
 
   constructor(props) {
     super(props);
-    this.state = {
-      turn: 'X',
-      gameInPlay: true,
-    }
-  }
-
-  gameWon = (newState) => {
-    newState.gameInPlay = false;
-    newState.winner = newState.turn;
-    return newState;
-  }
-
-  resetBoard = () => {
-    const brandNewState = {
-      turn: 'X',
-      gameInPlay: true,
-      cell0: null,
-      cell1: null,
+    // resetState is the beginning state, before the first move has been made.
+    // this.state is set to resetState and the beginning of the game and after
+    // the #reset button is clicked. Code on the reset button in render.
+    this.resetState = {
+      turn: 'X', // Turn flips between 'X' and 'O' and represents the player's mark
+      gameInPlay: true, // Starts as true. Set to false when game won. Set to true on reset.
+      // if gameInPlay is false, no more marks may be placed on the board until
+      // the game is reset, which toggles it back to true.
+      cell0: null, // (<div data-cell="0")
+      cell1: null, // (<div data-cell="1"), and so on...
       cell2: null,
       cell3: null,
       cell4: null,
@@ -29,15 +21,28 @@ class TicTacToe extends React.Component {
       cell6: null,
       cell7: null,
       cell8: null,
-      winner: null,
-    };
-    this.setState(brandNewState);
+      winner: null, // after game is won winner is set to the current turn
+    }
+    this.state = {...this.resetState};
+  }
+
+  // After a winning combination is found, gameInPlay is set to false so that no
+  // more marks can be made in the remaining cells. Winner is set to the current
+  // player's mark, either 'X' or 'O'.
+  gameWon = (newState) => {
+    newState.gameInPlay = false;
+    newState.winner = newState.turn;
+    return newState;
   }
 
   handleClick = (cell) => {
-    let newState = {...this.state};
-    if (!newState[cell] && newState.gameInPlay) {
+    if (!this.state[cell] && this.state.gameInPlay) {
+      // create temp newState to limit renderings as many state changes are made
+      let newState = {...this.state};
+      // Either an 'X' or 'O' placed in cell. Turn will be toggled if no winner found
       newState[cell] = newState.turn;
+      // switch will look for a win in the 8 possible winning combinations. If
+      // none are found, then its default will toggle the turn.
       switch (true) {
         case (newState.cell0 === newState.turn && newState.cell1 === newState.turn && newState.cell2 === newState.turn):
           newState = this.gameWon(newState);
@@ -64,8 +69,10 @@ class TicTacToe extends React.Component {
           newState = this.gameWon(newState);
           break;
         default:
+          // the move didn't cause a win, so the turn is toggled
           newState.turn = newState.turn === 'X' ? 'O' : 'X';
       }
+      // state updated after each valid move
       this.setState(newState);
     }
   };
@@ -91,7 +98,7 @@ class TicTacToe extends React.Component {
           </div>
           <div id="announce-winner">Winner: {this.state.winner}</div>
         </div>
-        <button id="reset" onClick={() => this.resetBoard()}>Play Again?</button>
+        <button id="reset" onClick={() => this.setState(this.resetState)}>Play Again?</button>
       </div>
     );
   }
