4d67147e6d01eb7311cfd8fdcba3842a8a994f6d
Leah Lawrence <lalawren@us.ibm.com>
2017-10-09 20:05:43 -0500

    (update) class notes tests.js

---
+++ 02week/tests.js
@@ -0,0 +1,45 @@
+'use strict';
+
+
+
+/*
+const assert = require('assert');
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+
+let add = function(a, b) {
+    let sum = a + b;
+    return sum;
+}
+
+if (typeof describe === 'function') {
+
+    describe('add()', () => {
+        it('testing add', () => {
+            assert.equal(add(0, 1), 1);
+            //assert.equal(add('banana', 'hamock'), 'Invalid Input');
+            assert.equal(add(Infinity, Infinity), Infinity);
+        });
+        it('testing add with infinity', () => {
+            assert.equal(add(Infinity, Infinity), Infinity);
+        });
+        it('testing add with negative', () => {
+            assert.equal(add(-5, -3), -8);
+        });
+        it('testing add with notEqual', () => {
+            assert.notEqual(add(5, '5'), 10);
+        });
+        it('testing add with notEqual', () => {
+            assert.notEqual(add(5, '5'), '10');
+        });
+        it('testing add with okay 1,0', () => {
+            assert.ok(add(1, 0));
+        });
+        it('testing add with assert', () => {
+            assert(!add(0, 0));
+        });
+    });
+}*/
