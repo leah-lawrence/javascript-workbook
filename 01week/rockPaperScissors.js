cda0d5a74e45a3d36a8e5eef8c6940a1fd991088
Leah Lawrence <lalawren@us.ibm.com>
2017-10-15 14:58:53 -0500

    (saving) homework4 pigLatin.js

--- 01week/rockPaperScissors.js
+++ 01week/rockPaperScissors.js
@@ -3,65 +3,135 @@
 const assert = require('assert');
 const readline = require('readline');
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
 function rockPaperScissors(hand1, hand2) {
 
-var hand1Wins = "HAND1 WINS " + hand1 + " beats " + hand2;
-var hand2Wins = "HAND2 WINS " + hand2 + " beats " + hand1;
-var noWinner = "No winner, try again";
+    let hand1Convert = hand1.toLowerCase().trim();
+    let hand2Convert = hand2.toLowerCase().trim();
 
-  if (hand1 === hand2) {
-    return noWinner;
-  } else if (hand1 === 'rock' && hand2 === 'paper') {
-    return hand2Wins;
-  } else if (hand1 === 'rock' && hand2 === 'scissors') {
-    return hand1Wins;
-  } else if (hand1 === 'paper' && hand2 === 'rock') {
-    return hand1Wins;
-  } else if (hand1 === 'paper' && hand2 === 'scissors') {
-    return hand2Wins;
-  } else if (hand1 === 'scissors' && hand2 === 'rock') {
-    return hand2Wins;
-  } else if (hand1 === 'scissors' && hand2 === 'paper') {
-    return hand1Wins;
-  }
+    var hand1Wins = "Hand one wins!";
+    var hand2Wins = "Hand two wins!";
+    var noWinner = "It's a tie!";
+
+    if (hand1Convert === hand2Convert) {
+        return noWinner;
+    } else if (hand1Convert === 'rock' && hand2Convert === 'paper') {
+        return hand2Wins;
+    } else if (hand1Convert === 'rock' && hand2Convert === 'scissors') {
+        return hand1Wins;
+    } else if (hand1Convert === 'paper' && hand2Convert === 'rock') {
+        return hand1Wins;
+    } else if (hand1Convert === 'paper' && hand2Convert === 'scissors') {
+        return hand2Wins;
+    } else if (hand1Convert === 'scissors' && hand2Convert === 'rock') {
+        return hand2Wins;
+    } else if (hand1Convert === 'scissors' && hand2Convert === 'paper') {
+        return hand1Wins;
+    }
 }
 
 function getPrompt() {
-  rl.question('hand1: ', (answer1) => {
-    rl.question('hand2: ', (answer2) => {
-      console.log( rockPaperScissors(answer1, answer2) );
-      getPrompt();
+    rl.question('hand1: ', (answer1) => {
+        rl.question('hand2: ', (answer2) => {
+            console.log(rockPaperScissors(answer1, answer2));
+            getPrompt();
+        });
     });
-  });
 }
 
 // Tests
 
 if (typeof describe === 'function') {
 
-  describe('#rockPaperScissors()', () => {
-    it('should detect a tie', () => {
-      assert.equal(rockPaperScissors('rock', 'rock'), "It's a tie!");
-      assert.equal(rockPaperScissors('paper', 'paper'), "It's a tie!");
-      assert.equal(rockPaperScissors('scissors', 'scissors'), "It's a tie!");
+    describe('#rockPaperScissors()', () => {
+        it('should detect a tie', () => {
+            assert.equal(rockPaperScissors('rock', 'rock'), "It's a tie!");
+            assert.equal(rockPaperScissors('paper', 'paper'), "It's a tie!");
+            assert.equal(rockPaperScissors('scissors', 'scissors'), "It's a tie!");
+        });
+        it('all scenarios for hand 1 winning', () => {
+            assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
+            assert.equal(rockPaperScissors('paper', 'rock'), "Hand one wins!");
+            assert.equal(rockPaperScissors('scissors', 'paper'), "Hand one wins!");
+        });
+        it('all scenarios for hand 2 winning', () => {
+            assert.equal(rockPaperScissors('rock', 'paper'), "Hand two wins!");
+            assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
+            assert.equal(rockPaperScissors('scissors', 'rock'), "Hand two wins!");
+        });
+        it('should scrub input to ensure toLowerCase', () => {
+            assert.equal(rockPaperScissors('ROCK', 'PAPER'), "Hand two wins!");
+            assert.equal(rockPaperScissors('ROCK', 'SCISSORS'), "Hand one wins!");
+            assert.equal(rockPaperScissors('PAPER', 'SCISSORS'), "Hand two wins!");
+            assert.equal(rockPaperScissors('PAPER', 'SCISSORS'), "Hand two wins!");
+            assert.equal(rockPaperScissors('SCISSORS', 'ROCK'), "Hand two wins!");
+            assert.equal(rockPaperScissors('SCISSORS', 'PAPER'), "Hand one wins!");
+        });
+        it('should scrub input to ensure trim', () => {
+            assert.equal(rockPaperScissors(' rock ', ' scissors '), "Hand one wins!");
+            assert.equal(rockPaperScissors(' paper ', ' rock '), "Hand one wins!");
+            assert.equal(rockPaperScissors(' scissors ', ' paper '), "Hand one wins!");
+            assert.equal(rockPaperScissors(' rock ', ' paper '), "Hand two wins!");
+            assert.equal(rockPaperScissors(' paper ', ' scissors '), "Hand two wins!");
+            assert.equal(rockPaperScissors(' scissors ', ' rock '), "Hand two wins!");
+        });
+        it('no funny business', () => {
+            assert.equal(rockPaperScissors('rOcK', 'sCiSsOrS'), "Hand one wins!");
+            assert.equal(rockPaperScissors('pApEr', 'rOcK'), "Hand one wins!");
+            assert.equal(rockPaperScissors('sCiSsOrS', 'pApEr'), "Hand one wins!");
+            assert.equal(rockPaperScissors('RoCk', 'PaPeR'), "Hand two wins!");
+            assert.equal(rockPaperScissors('PaPeR', 'ScIsSoRs'), "Hand two wins!");
+            assert.equal(rockPaperScissors('ScIsSoRs', 'RoCk'), "Hand two wins!");
+        });
+
     });
-    it('should detect which hand won', () => {
-      assert.equal(rockPaperScissors('rock', 'paper'), "Hand two wins!");
-      assert.equal(rockPaperScissors('paper', 'scissors'), "Hand two wins!");
-      assert.equal(rockPaperScissors('rock', 'scissors'), "Hand one wins!");
-    });
-    it('should scrub input to ensure lowercase with "trim"ed whitepace', () => {
-      assert.equal(rockPaperScissors('rOcK', ' paper '), "Hand two wins!");
-      assert.equal(rockPaperScissors('Paper', 'SCISSORS'), "Hand two wins!");
-      assert.equal(rockPaperScissors('rock ', 'sCiSsOrs'), "Hand one wins!");
-    });
-  });
 } else {
 
-  getPrompt();
+    getPrompt();
 
 }
+
+
+/*
+Class Notes
+To run a test in the terminal: npm test 02week/tests.js
+const assert = require('assert');
+const readline = require('readline');
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+});
+let add = function(a, b) {
+    let sum = a + b;
+    return sum;
+}
+if (typeof describe === 'function') {
+    describe('add()', () => {
+        it('testing add', () => {
+            assert.equal(add(0, 1), 1);
+            //assert.equal(add('banana', 'hamock'), 'Invalid Input');
+            assert.equal(add(Infinity, Infinity), Infinity);
+        });
+        it('testing add with infinity', () => {
+            assert.equal(add(Infinity, Infinity), Infinity);
+        });
+        it('testing add with negative', () => {
+            assert.equal(add(-5, -3), -8);
+        });
+        it('testing add with notEqual', () => {
+            assert.notEqual(add(5, '5'), 10);
+        });
+        it('testing add with notEqual', () => {
+            assert.notEqual(add(5, '5'), '10');
+        });
+        it('testing add with okay 1,0', () => {
+            assert.ok(add(1, 0));
+        });
+        it('testing add with assert', () => {
+            assert(!add(0, 0));
+        });
+    });
+}*/
--- 02week/pigLatin.js
+++ 02week/pigLatin.js
@@ -3,80 +3,71 @@
 const assert = require('assert');
 const readline = require('readline');
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
+function pigLatin(str) {
 
-function pigLatin(word) {
+    let fullWord = str.toLowerCase().trim(); // clean up the input
 
-  let fullWord = word.toString().toLowerCase().trim(); // Clean up the input
-  let firstHalf = fullWord.substring(0, 2); // Pull out first two letters of a string
+    var vowel = ['a', 'e', 'i', 'o', 'u']; // new arrays vowels
+    let firstLetter = fullWord[0]; // identifies first letter of word
+
+    if (vowel.includes(firstLetter)) {
+        // run vowel conditions  
+        return fullWord + 'yay';
+    } else {
+        // run consonant conditions
+        //for(var i = 1; fullWord.length; i++){
+
+        }
+    }
 
 }
 
-pigLagin();
-
+/*
+pigLatin('car'); // arcay
+pigLatin('dog'); // ogday
+pigLatin('create'); // eatecray
+pigLatin('valley'); // alleyvay
+pigLatin('egg'); // eggyay
+pigLatin('emission'); // emissionyay
+pigLatin('HeLlO '); // ellohay
+pigLatin(' RoCkEt'); // ocketray
+*/ 
 
 function getPrompt() {
-  rl.question('word ', (answer) => {
-    console.log( pigLatin(answer) );
-    getPrompt();
-  });
+    rl.question('word ', (answer) => {
+        console.log(pigLatin(answer));
+        getPrompt();
+    });
 }
 
 // Tests
 
 if (typeof describe === 'function') {
 
-  describe('#pigLatin()', () => {
-    it('should translate a simple word', () => {
-      assert.equal(pigLatin('car'), 'arcay');
-      assert.equal(pigLatin('dog'), 'ogday');
+    describe('#pigLatin()', () => {
+        it('should translate a simple word', () => {
+            assert.equal(pigLatin('car'), 'arcay');
+            assert.equal(pigLatin('dog'), 'ogday');
+        });
+        it('should translate a complex word', () => {
+            assert.equal(pigLatin('create'), 'eatecray');
+            assert.equal(pigLatin('valley'), 'alleyvay');
+        });
+        it('should attach "yay" if word begins with vowel', () => {
+            assert.equal(pigLatin('egg'), 'eggyay');
+            assert.equal(pigLatin('emission'), 'emissionyay');
+        });
+        it('should lowercase and trim word before translation', () => {
+            assert.equal(pigLatin('HeLlO '), 'ellohay');
+            assert.equal(pigLatin(' RoCkEt'), 'ocketray');
+        });
     });
-    it('should translate a complex word', () => {
-      assert.equal(pigLatin('create'), 'eatecray');
-      assert.equal(pigLatin('valley'), 'alleyvay');
-    });
-    it('should attach "yay" if word begins with vowel', () => {
-      assert.equal(pigLatin('egg'), 'eggyay');
-      assert.equal(pigLatin('emission'), 'emissionyay');
-    });
-    it('should lowercase and trim word before translation', () => {
-      assert.equal(pigLatin('HeLlO '), 'ellohay');
-      assert.equal(pigLatin(' RoCkEt'), 'ocketray');
-    });
-  });
 } else {
 
-  getPrompt();
+    getPrompt();
 
 }
-
-/*Consonants
-car — > archer
-
-Vowels
-exam —> xamyay
-
-let haystack = “Bob is a nice guy”;
-let needle = ‘nice’;
-console.log(haystack.indexOf(needle));
-// returns 9
-
-needle = 0;
-
-let fullWord = “straw”;
-let firstHalf = fullWord.substring(0, 3); // first half
-let secondHold = fullWord.substring(3,5); // full word
-let secondHold = fullWord.substring(3); // all the way to the end
-let howLongIsTheWord = fullWord.length;
-console.log(firstHalf);
-console.log(secondHalf);
-console.log(howLongIsTheWord);
-
-let translated = secondHalf + firstHalf + “ay”;
-console.log(translated);
-
-Indexof
-substring*/
