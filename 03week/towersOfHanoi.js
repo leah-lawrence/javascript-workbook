567939452432dd54570fb9eb3f7e827789b6caa4
Leah Lawrence <lalawren@us.ibm.com>
2017-10-22 14:20:05 -0500

    (saving) towersOfHanoi.js

--- 03week/towersOfHanoi.js
+++ 03week/towersOfHanoi.js
@@ -3,52 +3,82 @@
 const assert = require('assert');
 const readline = require('readline');
 const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
+    input: process.stdin,
+    output: process.stdout
 });
 
 let stacks = {
-  a: [4, 3, 2, 1],
-  b: [],
-  c: []
+    a: [4, 3, 2, 1],
+    b: [],
+    c: []
 };
 
+// Renaming each stack to a peg
+// Vars return as arrays
+// Auto sorts arrays with the lowest number always at the end
+let peg1 = stacks.a.reverse();
+let peg2 = stacks.b.reverse();
+let peg3 = stacks.c.reverse();
+
+let pegNames = ['a', 'b', 'c'];
+
 function printStacks() {
-  console.log("a: " + stacks.a);
-  console.log("b: " + stacks.b);
-  console.log("c: " + stacks.c);
+    console.log("a: " + peg1);
+    console.log("b: " + peg2);
+    console.log("c: " + peg3);
 }
 
 function movePiece() {
-  // Your code here
+    // If the 
 
 }
 
-function isLegal() {
-  // Your code here
+function isLegal(start, end) {
+
+    // Are the user inputs valid? 
+    if (!pegNames.includes(start) || !pegNames.includes(end)) {
+        console.log("Invalid Stack Specified");
+        return;
+    }
 
 }
 
 function checkForWin() {
-  // Your code here
 
+    // Win condition
+    let win = [4, 3, 2, 1];
+
+    // Are all of the numbers in order at the end of the stack?
+    // 
+    if (peg3 === win) {
+        // Return True/as won
+    } else {
+        // Return False/as lost
+    }
 }
 
 function towersOfHanoi(startStack, endStack) {
-  // Your code here
+
+    // Are the user inputs valid? 
+    let start = startStack;
+    let end = endStack;
+    isLegal(start, end);
+
+    // Move piece
+
+    // Check for win
+    checkForWin();
 
 }
 
 function getPrompt() {
-  printStacks();
-  rl.question('start stack: ', (startStack) => {
-    rl.question('end stack: ', (endStack) => {
-      towersOfHanoi(startStack, endStack);
-      getPrompt();
+    printStacks();
+    rl.question('start stack: ', (startStack) => {
+        rl.question('end stack: ', (endStack) => {
+            towersOfHanoi(startStack, endStack);
+            getPrompt();
+        });
     });
-  });
 }
 
 getPrompt();
-
-
