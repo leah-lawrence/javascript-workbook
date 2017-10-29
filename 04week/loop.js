f5587dd544c00cd6d513b1eaa9b7460e60fcf5a0
Leah Lawrence <lalawren@us.ibm.com>
2017-10-29 12:16:26 -0500

    (update) loops homework complete loops.js

--- 04week/loop.js
+++ 04week/loop.js
@@ -6,8 +6,8 @@
 
 // Use a for loop to console.log each item in the array carsInReverse.
 let carsInReverse = ['cadillac', 'bentley', 'accord'];
-for (let index in carsInReverse) {
-    console.log(carsInReverse[index]);
+for (let i = 0; i < carsInReverse.length; i++) {
+    console.log(carsInReverse[i]);
 }
 
 ///////////////////////
@@ -28,20 +28,16 @@ for (let index in persons) {
 }
 
 // then use a for...in loop and if state to console.log the value associated with the key birthDate.
-// ????
-for (let index in persons) {
-	console.log(persons[index]);
-    if (index == persons["birthDate"]) {
-        console.log(persons["birthDate"]);
-    }
+for (let name in persons) {
+    
+    if (name !== 'birthDate' ) {};
+    
+    if (name === 'birthDate' ) {
+    console.log(persons['birthDate']);
+    };
+    
 }
 
-for (let index in persons) {
-	let stopCondition = persons["birthDate"];
-    if (index === stopCondition) {
-        console.log(stopCondition);
-    }
-}
 
 ///////////////////////
 //// WHILE LOOP
@@ -69,10 +65,23 @@ do {
 
 // When is a for loop better than a while loop?
 
+A for..loop is used if the number of loops is known. 
+A while..loop is used if the number of loops is unknown. 
+
 // How is the readability of the code affected?
+
+A while loop only needs the complete condition, while 
+the for..loop requires initialization, a step operation, 
+and complete condition. 
 
 // What is the difference between a for loop and a for...in loop?
 
+For..in loops are used to iterate through the properties of 
+objects, while for..loops are used for sequential arrays. 
+
 // What is the difference between a while loop and a do...while loop?
+
+A do..while loop tests the condition at the end of the loop, and will
+run the code while the condition is true. 
 
 ************************************/
