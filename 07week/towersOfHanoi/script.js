7c4a7d4830616a2e411533b27f569b40fd540119
Leah Lawrence <lalawren@us.ibm.com>
2017-11-18 14:24:40 -0600

    (saving) towersOfHanoi/script.js

--- 07week/towersOfHanoi/script.js
+++ 07week/towersOfHanoi/script.js
@@ -3,45 +3,52 @@
 class TowersOfHanoi extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {
-        block: null, 
-        board: {
+    this.state = { 
+      block: null, 
+      board: {
          1: [100,75,50,25],
          2: [],
          3: []
         }
-    }
+    };
     this.playerTurn = this.playerTurn.bind(this);
   }
-  // onClick, select data-block and put playerTurn in a data-stack
+
   playerTurn(event){
       console.log("✓ playerTurn()");
 
       const stack = event.target.getAttribute("data-stack"); 
-      const disk = event.target.getAttribute("data-block");
-      const board = this.state.board;
-      // console.log(stack);
-      // console.log(disk);
-      // console.log(board);
+      const newStack = this.state[stack].slice(); // make a copy of the array to remove items
+      const obj = {}; // create an empty object to hold stack and block variables
+      obj[stack] = newStack;
 
-      // adds disk to this.state.board - onClick
-      
+      // add disk to stack - onClick
+      if (!this.state.block) { // if there is a block in the stack
+        const block = newStack.pop(); //remove the last element and assign to block
+        obj['block'] = block;
+      } else if (newStack.length === 0 || this.state.block < this.state[stack][this.state[stack].length-1]) {
+        newStack.push(this.state.block); //place the block on stack
+        obj['block'] = null; //reset block value
+      }
+
+      this.setState(obj); 
   }
-
 
   render() {
 
-    const block1 = this.state.board[1].map((block) => { 
-      return (<div data-block={block}></div>);
-    });
+  const board = this.state.board;
 
-    const block2 = this.state.board[2].map((block) => {
-      return (<div data-block={block}></div>);
-    });
+  const block1 = board[1].map((block) => { 
+    return (<div data-block={block}></div>);
+  });
 
-    const block3 = this.state.board[3].map((block) => {
-      return (<div data-block={block}></div>);
-    });
+  const block2 = board[2].map((block) => {
+    return (<div data-block={block}></div>);
+  });
+
+  const block3 = board[3].map((block) => {
+    return (<div data-block={block}></div>);
+  });
 
     return (
       <div>
