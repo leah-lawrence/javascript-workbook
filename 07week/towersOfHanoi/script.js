1f67d8480c59ad2ff95982963640b9d07ceed23b
Leah Lawrence <lalawren@us.ibm.com>
2017-11-20 23:09:00 -0600

    (saving) homework complete towersOfHanoi/script.js

--- 07week/towersOfHanoi/script.js
+++ 07week/towersOfHanoi/script.js
@@ -6,14 +6,14 @@ class TowersOfHanoi extends React.Component {
     this.state = { 
       block: null, 
       board: {
-         1: [100,75],
-         2: [50,25],
-         3: []
-        }
+        1: [100,75, 50,25],
+        2: [],
+        3: []
+      }
     };
     this.playerTurn = this.playerTurn.bind(this);
   }
-
+  // move disks on board
   playerTurn(event){
       const board = this.state.board; 
       const block = this.state.block;
@@ -21,20 +21,20 @@ class TowersOfHanoi extends React.Component {
       const selStack = board[stack] // selected stack ie this.state.board[1]
       const store = {};
 
-      // add disk to stack - onCli£ck
+      // add disk to stack - onClick
       if (!block) { // if the stack has a block
         const move = selStack.pop(); // take last element in selected board's array
         this.state.block = move; // move to this.state.block
         store['move'] = move; // move to store
-        console.log(this.state); // print the state of the object in console
-        console.log("if runs"); // console test
-      } else if (selStack.length === 0 || selStack[selStack.length-1] > block) {
+        // console.log(this.state); // print the state of the object in console
+        // console.log("if runs"); // console test
+      } else if (selStack.length === 0 || selStack[selStack.length-1] > block) { // if nothing in stack, or last item in stack is less than stored block
         selStack.push(this.state.block); // place the block on stack
-        store['block'] = null; // reset
-        console.log("else if runs") // console test
+        this.state.block = null; // reset
+        // console.log("else if runs") // console test
       }
-
-      this.setState(store);
+      this.setState(store); // change 
+      console.log(this.state);
   }
 
   render() {
