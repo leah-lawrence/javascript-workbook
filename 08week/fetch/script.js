f256218d9fcdc263e77b94c967e284e4b2717d4e
Leah Lawrence <lalawren@us.ibm.com>
2017-11-29 18:55:07 -0600

    (saving) fetch/script.js

--- 08week/fetch/script.js
+++ 08week/fetch/script.js
@@ -1,1 +1,15 @@
 'use strict';
+
+class ListUsers extends React.Component {
+	constructor(props){
+		super(props);
+	}
+
+	render(){
+		return(
+			<div> hello world </div>
+		)
+	}
+}
+
+ReactDOM.render(<ListUsers/>, document.getElementByID('fetch'));
