dcc1f2aa84f1de7babe1fbefb3b867c66272428a
Leah Lawrence <lalawren@us.ibm.com>
2017-12-04 20:42:10 -0600

    (saving) hackernews/script.js

--- 09week/hackernews/script.js
+++ 09week/hackernews/script.js
@@ -9,22 +9,26 @@ class Hacker extends React.Component {
 	}
 
 	componentWillMount(){
+		// store state changes
 		const stories = []
+		// create list 
 		for (let i=1; i<11; i++) {
-			const url = 'https://hacker-news.firebaseio.com/v0/item/506.json?print=pretty';
+			const url = `https://hacker-news.firebaseio.com/v0/item/${i}.json?print=pretty `; // Hacker News API
+			console.log("Fetch Object", URL);
 			const options = {
 				method: 'GET',
 				headers: {
 					'content-type': 'application/json'
 				}
 			}
+			// call API
 			fetch(url, options).then((result) => {
 			return result.json();
 			}).then((response) => {			
-				stories.push(response);
-				if(stories.length === 10){
-					this.setState({
-						list: stories
+				stories.push(response); // send information to storage
+				if(stories.length === 10){ 
+					this.setState({ 
+						list: stories // set state
 					})
 				}
 			});
@@ -33,13 +37,21 @@ class Hacker extends React.Component {
 
 	render(){
 		return(
-			<ul>
+			<ol>
 			{
+				// display list in the browser
 				this.state.list.map((item, index) => {
-					return <li key={index}> {item.title} </li>
+					console.log(item.title); // show titles in console
+					console.log(item.url); // show urls in console
+
+					return <li key={index}> {item.title} <br /> 	
+					By: {item.by} <br />							
+					<a href={item.url} target="_blank"> Go to article</a>
+					<br /><br />
+					</li>	
 				})
 			}
-			</ul>
+			</ol>
 		)
 	}
 }
