5e23c141461bfe3e3ecf9dcc514eac943d6b47f7
Leah Lawrence <lalawren@us.ibm.com>
2017-12-04 19:50:14 -0600

    (saving) hackernews/script.js

--- 09week/hackernews/script.js
+++ 09week/hackernews/script.js
@@ -1,1 +1,47 @@
 'use strict';
+
+class Hacker extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {
+			list: []
+		}
+	}
+
+	componentWillMount(){
+		const stories = []
+		for (let i=1; i<11; i++) {
+			const url = 'https://hacker-news.firebaseio.com/v0/item/506.json?print=pretty';
+			const options = {
+				method: 'GET',
+				headers: {
+					'content-type': 'application/json'
+				}
+			}
+			fetch(url, options).then((result) => {
+			return result.json();
+			}).then((response) => {			
+				stories.push(response);
+				if(stories.length === 10){
+					this.setState({
+						list: stories
+					})
+				}
+			});
+		}
+	}
+
+	render(){
+		return(
+			<ul>
+			{
+				this.state.list.map((item, index) => {
+					return <li key={index}> {item.title} </li>
+				})
+			}
+			</ul>
+		)
+	}
+}
+
+ReactDOM.render(<Hacker />, document.getElementById('hacker-news'));
