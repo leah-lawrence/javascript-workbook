59f5ececc00bc7e079ad334fda86076c5cd31acc
conorf <conorfleming@outlook.com>
2017-12-06 19:37:16 -0600

    first commit

--- 09week/hackernews/script.js
+++ 09week/hackernews/script.js
@@ -1,1 +1,45 @@
 'use strict';
+
+class HackerNews extends React.Component {
+  constructor(props) {
+    super(props);
+    this.set = this.thingthing.bind(this);
+    this.state = {
+      list: null,
+      notUpdated: true
+    }
+  }
+
+  thingthing(arr) {
+    let p = fetch('https://hacker-news.firebaseio.com/v0/item/8870.json?print=pretty');
+    let jsonPromise = p.then(function(response) {
+      console.log("the status of my response: ", response.status);
+      return response.json();
+    });
+
+    let x = jsonPromise.then((data) => {
+      let arr = data;
+      this.setState({
+        list: arr,
+        notUpdated: false
+      });
+    });
+  }
+
+  render() {
+    if (this.state.notUpdated) {
+      this.thingthing();
+    };
+
+    let list = this.state.list;
+
+    return (
+      <div className="container-fluid">
+          <h1>Hello! I do work!</h1>
+        <div className="row">{this.state.notUpdated ? "hello!" : list.by}</div>
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<HackerNews />, document.getElementById('hacker-news'));
