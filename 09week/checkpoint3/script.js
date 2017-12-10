7a5cf8efa9e1ac848bdb4fd88e4d4271869d9c4d
Leah Lawrence <lalawren@us.ibm.com>
2017-12-10 16:31:09 -0600

    (saving) API Request and information manipulation complete checkpoint3/script.js

--- 09week/checkpoint3/script.js
+++ 09week/checkpoint3/script.js
@@ -47,18 +47,40 @@ class BitcoinComponent extends React.Component {
 	// Display data on button click
 	displayData(event) {
 		if (Object.keys(this.state.apiData).length) {
-			const USD = ["USD " + this.state.apiData.USD.symbol, 
+			const USD = ["Currency " + this.state.apiData.USD.symbol, 
 						 "Buy: " + this.state.apiData.USD.buy, 
 						 "Sell: " + this.state.apiData.USD.sell,
 						 "Last: " + this.state.apiData.USD.last];
-
+			const JPY = ["Currency " + this.state.apiData.JPY.symbol, 
+						 "Buy: " + this.state.apiData.JPY.buy, 
+						 "Sell: " + this.state.apiData.JPY.sell,
+						 "Last: " + this.state.apiData.JPY.last];
+			const EUR = ["Currency " + this.state.apiData.EUR.symbol, 
+						 "Buy: " + this.state.apiData.EUR.buy, 
+						 "Sell: " + this.state.apiData.EUR.sell,
+						 "Last: " + this.state.apiData.EUR.last];
+			const NZD = ["Currency " + this.state.apiData.NZD.symbol, 
+						 "Buy: " + this.state.apiData.NZD.buy, 
+						 "Sell: " + this.state.apiData.NZD.sell,
+						 "Last: " + this.state.apiData.NZD.last];
 	        // display selected currency data
 			const selCurrency = event.target.getAttribute("data-name");
-			this.setState( {selectedCurrency:USD} );
+			if (selCurrency === 'USD' ){
+				this.setState({selectedCurrency:null});
+				this.setState({selectedCurrency:USD});
+			} else if (selCurrency === 'JPY' ){
+				this.setState({selectedCurrency:null});
+				this.setState({selectedCurrency:JPY});
+			} else if (selCurrency === 'EUR' ){
+				this.setState({selectedCurrency:null});
+				this.setState({selectedCurrency:EUR});
+			} else if (selCurrency === 'NZD' ){
+				this.setState({selectedCurrency:null});
+				this.setState({selectedCurrency:NZD});
+			};
 		
 			// console log clicked event
 			console.log("vvvvvv ONCLICK EVENT vvvvvv");
-			console.log("USD Array ", USD);
 	        console.log("Element Value: ", event.target.getAttribute("data-name"));
 			console.log("New selectedCurrency State: ", this.state.selectedCurrency);
 		}	
